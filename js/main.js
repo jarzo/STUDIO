6ff2c283978b221a6277ce08cb29de6d035c2726
Michał Jarząbek <nebraska994@gmail.com>
2017-10-18 23:13:52 +0200

    4 zakładki

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 'use strict';
+$('.carousel').carousel({
+  interval: 3000
+})
+
+$(".blok").click(function (event) {
+	event.preventDefault();
+});
+
+
+$(document).ready(function () {
+	$('[data-toggle="popover"]').popover();
+});
